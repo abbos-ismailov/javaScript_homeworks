727adde07350adb943307ec970e92e5d3860d671
Abbos Ismailov <abbosismailov0017@gmail.com>
2022-12-11 00:04:42 +0500

    The end

--- javaScript 6-dars/soz-son/soz-son.js
+++ javaScript 6-dars/soz-son/soz-son.js
@@ -1,6 +1,7 @@
 let words = document.getElementById('words');
 let btn = document.getElementById('btn');
 let h2 = document.getElementById('h2');
+
 btn.addEventListener('click', () => {
    let array = words.value.split(' ');
    h2.innerHTML = array.length;

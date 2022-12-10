c35c95e8a46da170a1d6810e974723e0f5a2e5a7
Abbos Ismailov <abbosismailov0017@gmail.com>
2022-12-11 00:02:37 +0500

    tugadi

--- javaScript 6-dars/ism-sorash/ism-topish.js
+++ javaScript 6-dars/ism-sorash/ism-topish.js
@@ -1,10 +1,10 @@
 let odamlar_name = prompt('5 ta ot kiriting');
 let array = odamlar_name.split(' ');
 for (let i = 0; i < array.length; i++){
-      if (array[i].toLowerCase().includes('abdu') === true) {
+      if (array[i].toLowerCase().includes('abdu')) {
             console.log(array[i]);
       }
-      else if (array[i].toLowerCase().includes('gul') === true) {
+      else if (array[i].toLowerCase().includes('gul')) {
             console.log(array[i]);
       }
       else{
--- javaScript 6-dars/soz-son/soz-son.js
+++ javaScript 6-dars/soz-son/soz-son.js
@@ -1,5 +1,7 @@
 let words = document.getElementById('words');
-words.addEventListener('input', () => {
-   let array = words.value
-   
+let btn = document.getElementById('btn');
+let h2 = document.getElementById('h2');
+btn.addEventListener('click', () => {
+   let array = words.value.split(' ');
+   h2.innerHTML = array.length;
 });
